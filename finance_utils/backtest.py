b679395571b13128743ec3388cb63c5f8057d09f
henrywzh <henrywzh88@gmail.com>
2024-07-13 19:35:18 +0800

    update: backtest

--- finance_utils/backtest.py
+++ finance_utils/backtest.py
@@ -1,8 +1,8 @@
 from utils import *
-
+import yfinance as yf
 
 class Backtest:
-    def __init__(self, data: pd.DataFrame, start_date, end_date):
+    def __init__(self, data: pd.DataFrame, start_date, end_date, benchmark: str = 'SPY', r_f=None):
         """
         :param data: initial columns: Value, Returns
         :param start_date:
@@ -20,6 +20,8 @@ class Backtest:
         if end_date is None or end_date > data.index[-1]:
             end_date = data.index[-1]
 
+        self.r_f = 0 if r_f is None else r_f
+        self.benchmark = benchmark
         self.df = data  # assumes results initially contains the returns & Value
         self.start_date = start_date
         self.end_date = end_date
@@ -29,7 +31,34 @@ class Backtest:
 
     # ---- The main function ----
     def run(self) -> None:  # run the backtest
-        pass
+        # -- clean all stuff first --
+        self.reset()
+
+        # -- adding stuff to the df --
+        self.df['Peak'] = self.peak()
+        self.df['Drawdown'] = self.drawdown()
+
+        # -- adding stuff to the results --
+        self.results['Initial Value'] = self.initial_value()
+        self.results['Peak Value'] = self.peak_value()
+        self.results['Final Value'] = self.final_value()
+        self.results['Max Drawdown'] = self.max_drawdown()
+        self.results['Avg Drawdown'] = self.avg_drawdown()
+        self.results['Calmar Ratio'] = self.calmar_ratio()
+        self.results['Sterling Ratio'] = self.sterling_ratio()
+        self.results['Annualised Return'] = self.annualised_return()
+        self.results['Volatility'] = self.volatility()
+        self.results['Sharpe Ratio'] = self.sharpe_ratio()
+        self.results['Downside Volatility'] = self.downside_volatility()
+        self.results['Sortino Ratio'] = self.sortino_ratio()
+        self.results['VaR 95'] = self.value_at_risk(alpha=95)
+        self.results['VaR 99'] = self.value_at_risk(alpha=99)
+        self.results['CVaR 95'] = self.conditional_VaR(alpha=95)
+        self.results['CVaR 99'] = self.conditional_VaR(alpha=99)
+        alpha, beta, r_2 = self.alpha_beta_r(self.benchmark)
+        self.results['Alpha'] = alpha
+        self.results['Beta'] = beta
+        self.results['R^2'] = r_2
 
     def get_results(self) -> dict:  # get results after run
         return self.results
@@ -40,6 +69,12 @@ class Backtest:
     def plot(self):
         pass
         # TODO: Plot the backtest result
+
+    def reset(self):
+        self.results = dict()
+        columns = self.df.columns.tolist()[2:]
+        self.df = self.df.drop(columns=columns)
+        print("df & results cleaned")
 
     # ---- Single value functions ----
     def initial_value(self) -> float:
@@ -70,7 +105,39 @@ class Backtest:
     def annualised_return(self) -> float:
         return get_annual_return(self.df['Return'])
 
-    # TODO: Store these values into the dict
+    def volatility(self) -> float:
+        return get_volatility(self.df['Return'])
+
+    def sharpe_ratio(self) -> float:
+        return get_sharpe_ratio(self.df['Return'], self.r_f)
+
+    def downside_volatility(self) -> float:
+        return get_volatility(get_downside_returns(self.df['Return']))
+
+    def sortino_ratio(self) -> float:
+        return get_sortino_ratio(self.df['Return'], self.r_f)
+
+    def value_at_risk(self, alpha=99, lookback_days=None) -> float:
+        return get_VaR(self.df['Return'], alpha, lookback_days)
+
+    def conditional_VaR(self, alpha=99, lookback_days=None) -> float:
+        return get_CVaR(self.df['Return'], alpha, lookback_days)
+
+    def alpha_beta_r(self, benchmark: str) -> (float, float, float):
+        benchmark_df = yf.download(benchmark, start=self.start_date, end=self.end_date)
+        benchmark_returns = benchmark_df['Adj Close'].pct_change()
+        beta, alpha, r_2, _, _ = stats.linregress(self.df['Return'], benchmark_returns)
+        return alpha, beta, r_2
+
+    def alpha(self, benchmark: str) -> float:
+        alpha, _, _ = self.alpha_beta_r(benchmark)
+        return alpha
+
+    def beta(self, benchmark: str) -> float:
+        _, beta, _ = self.alpha_beta_r(benchmark)
+        return beta
+
+    # TODO: get r_f from yf directly instead of users' input
 
     # ---- time series value functions ----
     def peak(self) -> pd.Series:
