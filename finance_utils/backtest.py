feat: added monthly volatility heatmap, debug: the correct formula for return & volatility

finance_utils/backtest.py
@@ -21,8 +21,6 @@ class Backtest:
         :param r_f:
         """
 
-        # TODO: Change all the format to percentage (%)
-
         # -- format check --
         if 'Value' not in data.columns or 'Return' not in data.columns or 'Price' not in data.columns:
             raise ValueError('data columns should contain: Value, Return, Price')
@@ -61,15 +59,9 @@ class Backtest:
         self.df['Drawdown'] = self.drawdown()
 
         # -- adding stuff to the results --
-        self.results['Initial Value'] = self.initial_value()
-        self.results['Peak Value'] = self.peak_value()
-        self.results['Final Value'] = self.final_value()
-        self.results['Max Drawdown'] = self.max_drawdown()
-        self.results['Avg Drawdown'] = self.avg_drawdown()
-        self.results['Calmar Ratio'] = self.calmar_ratio()
-        self.results['Sterling Ratio'] = self.sterling_ratio()
-        self.results['Annualised Return'] = self.annualised_return()
-        self.results['Volatility'] = self.volatility()
+        self.results['Annualised Return (Geo)'] = self.annualised_return()
+        self.results['Avg Annual Return (Ari)'] = self.avg_annual_return()
+        self.results['Volatility (Std)'] = self.volatility()
         self.results['Sharpe Ratio'] = self.sharpe_ratio()
         self.results['Downside Volatility'] = self.downside_volatility()
         self.results['Sortino Ratio'] = self.sortino_ratio()
@@ -79,6 +71,14 @@ class Backtest:
         self.results['CVaR 95'] = self.conditional_VaR(alpha=95)
         self.results['CVaR 99'] = self.conditional_VaR(alpha=99)
 
+        self.results['Initial Value'] = self.initial_value()
+        self.results['Peak Value'] = self.peak_value()
+        self.results['Final Value'] = self.final_value()
+        self.results['Max Drawdown'] = self.max_drawdown()
+        self.results['Avg Drawdown'] = self.avg_drawdown()
+        self.results['Calmar Ratio'] = self.calmar_ratio()
+        self.results['Sterling Ratio'] = self.sterling_ratio()
+
         alpha, beta, r_2 = self.alpha_beta_r()
         self.results['Alpha'] = alpha
         self.results['Beta'] = beta
@@ -86,8 +86,10 @@ class Backtest:
 
         self.results['Monthly Return'] = self.calendar_month_return()
         self.results['Yearly Return'] = self.calendar_year_return()
+        self.results['Monthly Volatility'] = self.calendar_month_volatility()
 
         print("Backtesting completed")
+        self._print_results()
         self.plot()
 
     def get_results(self) -> dict:  # get results after run
@@ -98,14 +100,15 @@ class Backtest:
 
     # ---- Visualisation ----
 
-    def plot(self):
+    def plot(self) -> None:
         self.plot_cumulative_returns()
         self.plot_drawdown()
         self.plot_volatility()
         self.plot_monthly_return()
         self.plot_yearly_return()
+        self.plot_monthly_volatility()
 
-    def plot_cumulative_returns(self):
+    def plot_cumulative_returns(self) -> None:
         fig, ax = plt.subplots(figsize=(10, 6))
         ax.plot(100 * self.df['Benchmark'] / self.df['Benchmark'].iloc[0] - 1, label='Benchmark')
         ax.plot(100 * self.df['Value'] / self.df['Value'].iloc[0] - 1, label='Strategy')
@@ -113,14 +116,14 @@ class Backtest:
         plt.legend(loc='best')
         plt.show()
 
-    def plot_drawdown(self):
+    def plot_drawdown(self) -> None:
         fig, ax = plt.subplots(figsize=(10, 6))
         ax.plot(self.df['Drawdown'] * 100)
         ax.set(xlabel='Date', ylabel='Drawdown (%)', title='Drawdown')
         plt.legend(loc='best')
         plt.show()
 
-    def plot_volatility(self, rolling_window: int = 30):
+    def plot_volatility(self, rolling_window: int = 30) -> None:
         vol_df = self.rolling_volatility(rolling_window)
 
         fig, ax = plt.subplots(figsize=(10, 6))
@@ -129,14 +132,24 @@ class Backtest:
         plt.legend(loc='best')
         plt.show()
 
-    def plot_monthly_return(self):
-        plot_return_heatmap(self.results['Monthly Return'])
+    def plot_monthly_return(self) -> None:
+        plot_heatmap(self.results['Monthly Return'])
 
-    def plot_yearly_return(self):
+    def plot_yearly_return(self) -> None:
         plot_yearly_return(self.results['Yearly Return'])
 
+    def plot_monthly_volatility(self) -> None:
+        plot_heatmap(self.results['Monthly Volatility'])
+
+    def _print_results(self) -> None:
+        for key in self.results:
+            if isinstance(self.results[key], pd.Series) or isinstance(self.results[key], pd.DataFrame):
+                continue
+
+            print(f'{key}: {self.results[key]}')
+
     # ---- Reset parameters ----
-    def reset(self):
+    def reset(self) -> None:
         self.results = dict()
         columns = self.df.columns.tolist()[3:]  # removes all the columns besides from (Price, Value, Return)
         self.df = self.df.drop(columns=columns)
@@ -154,23 +167,25 @@ class Backtest:
             raise ValueError('Benchmark not found on Yahoo Finance')
 
     # ---- Changing attributes ----
-    def set_start_date(self, start_date: str):
+    def set_start_date(self, start_date: str) -> None:
         self.start_date = start_date
         self._check_date()
 
-    def set_end_date(self, end_date: str):
+    def set_end_date(self, end_date: str) -> None:
         self.end_date = end_date
         self._check_date()
 
-    def set_benchmark(self, benchmark: str):
+    def set_benchmark(self, benchmark: str) -> None:
         self.benchmark = benchmark
-        # TODO: add benchmark data to self.df
 
-    def set_benchmark_to_buy_and_hold(self):
+        # rerun the backtest
+        self.run()
+
+    def set_benchmark_to_buy_and_hold(self) -> None:
         self.benchmark = 'Price'
 
     # ---- Error Check ----
-    def _check_date(self):
+    def _check_date(self) -> None:
         if self.start_date > self.end_date:
             raise ValueError('Error: start_date > end_date')
 
@@ -214,8 +229,17 @@ class Backtest:
         # TODO
         pass
 
-    def annualised_return(self) -> float:
-        return get_annual_return(self.df['Return'])
+    def annualised_return(self, geo: bool = True) -> float:
+        return get_annual_return(self.df['Return'], geo=geo)
+
+    def avg_annual_return(self) -> float:
+        """
+        Calculates the arithmetic average annual return by calendar year
+        :return:
+        """
+        yearly_returns = self.calendar_year_return()
+
+        return yearly_returns.mean()
 
     def volatility(self) -> float:
         return get_volatility(self.df['Return'])
@@ -273,8 +297,11 @@ class Backtest:
     def calendar_year_return(self) -> pd.Series:
         return yearly_return(self.df['Price'])
 
+    def calendar_month_volatility(self) -> pd.Series:
+        return monthly_volatility(self.df['Return'])
+
     def rolling_volatility(self, windows: int = 30) -> pd.Series:
-        return (self.df['Return'] * 100).rolling(windows).std(ddof=1)
+        return (self.df['Return'] * 100).rolling(windows).std(ddof=1) * np.sqrt(TRADING_DAYS)
 
 
     """

finance_utils/utils.py
@@ -35,6 +35,11 @@ REMINDER:
 - sortino ratio = (r_i - r_f) / downside volatility
 """
 
+# ---- constants -----
+TRADING_DAYS: int = 252
+TRADING_MONTHS: int = 12
+TRADING_QUARTER: int = 4
+
 
 # assumes we have a dataframe of returns:
 def get_alpha_beta(returns: pd.Series, benchmark: str = 'SPY') -> (float, float, float):
@@ -55,30 +60,22 @@ def get_upside_returns(returns: pd.Series, threshold=0) -> pd.Series:
 
 # ---- return type: single value ----
 def get_volatility(returns: pd.Series) -> float:
-    if returns.abs().max() < 1:
-        temp_df = returns * 100
-    else:
-        temp_df = returns.copy()
+    temp_df = returns.dropna(axis=0)
 
-    temp_df = temp_df.dropna(axis=0)
-
-    return temp_df.std(ddof=1)
+    return temp_df.std(ddof=1) * np.sqrt(TRADING_DAYS)
 
 
 def get_downside_volatility(returns: pd.Series, threshold: int | float = 0) -> float:
-    # TODO: threshold ???
-    if returns.abs().max() < 1:
-        returns = returns * 100
-    else:
-        returns = returns.copy()
-
     returns = returns.dropna(axis=0)
     n = returns.shape[0]
-    return np.sqrt(sum([max(r_i, threshold) ** 2 for r_i in returns]) / (n - 1))
+    downside_vol = np.sqrt(sum([min(r_i - threshold, 0) ** 2 for r_i in returns]) / (n - 1))
+
+    return downside_vol * np.sqrt(TRADING_DAYS)
 
 
-def get_annual_return(returns: pd.Series, freq: str = 'D') -> float:
+def get_annual_return(returns: pd.Series, freq: str = 'D', geo: bool = True) -> float:
     """
+    :param geo:
     :param returns: the returns of the stock with a frequency freq
     :param freq: D | M | Q
     :return: gives out the compound annual return
@@ -87,18 +84,33 @@ def get_annual_return(returns: pd.Series, freq: str = 'D') -> float:
     freq = freq.upper()
 
     if freq == 'D':
-        period = 252
+        period = TRADING_DAYS
     elif freq == 'M' or freq == 'ME':
-        period = 12
+        period = TRADING_MONTHS
     elif freq == 'Q':
-        period = 4
+        period = TRADING_QUARTER
     else:
         raise ValueError('Does not support this kind of freq')
 
-    time_count = returns.shape[0]
-    returns = percent_to_num(returns)
+    returns = returns.dropna(axis=0)
+    time_count = returns.shape[0] - 1
 
-    return (np.cumprod(1 + returns) - 1).iloc[-1] ** (period / time_count)
+    cum_return = np.cumprod(1 + returns)
+
+    if geo:
+        annual_return = cum_return.iloc[-1] ** (period / time_count) - 1
+    else:
+        annual_return = cum_return.iloc[-1] * period / time_count
+
+    return annual_return
+
+
+def monthly_volatility(df: pd.Series) -> pd.Series:
+    """
+    :param df: pd.Series, the return of the stock
+    :return: pd.Series, the monthly volatility of the stock
+    """
+    return ((df * 100).resample('ME').std(ddof=1) * np.sqrt(TRADING_DAYS)).rename('Monthly Volatility')
 
 
 def monthly_return(df: pd.Series) -> pd.Series:
@@ -111,32 +123,32 @@ def monthly_return(df: pd.Series) -> pd.Series:
     return monthly.rename("Monthly Return")
 
 
-def plot_return_heatmap(_monthly_return: pd.Series, annot: bool = True):
+def plot_heatmap(_monthly_df: pd.Series, annot: bool = True):
     """
     :param annot:
-    :param _monthly_return: either daily price or monthly return of the stock
+    :param _monthly_df: monthly return | monthly volatility
     :return: a heatmap
     """
 
-    # -- check input --
-    if _monthly_return.name != 'Monthly Return':
-        _monthly_return = monthly_return(_monthly_return)
+    # # -- check input --
+    # if _monthly_df.name == 'Monthly Return':
+    #
 
     # -- format check --
-    _monthly_return = num_to_percent(_monthly_return)
+    _monthly_df = num_to_percent(_monthly_df)
 
     # -- reshaping --
-    _df = pd.DataFrame(_monthly_return)
+    _df = pd.DataFrame(_monthly_df)
     _df['Year'] = _df.index.year
     _df['Month'] = _df.index.month
     _df.index = [i for i in range(_df.shape[0])]
 
     # -- drawing the heatmap --
-    result = _df.pivot(index='Year', columns='Month', values='Monthly Return')
+    result = _df.pivot(index='Year', columns='Month', values=_monthly_df.name)
     fig, ax = plt.subplots(figsize=(10, 6))
 
     ax = sns.heatmap(result, linewidths=0.30, annot=annot, center=0)
-    plt.title("Calendar Return (%)")
+    plt.title(f"{_monthly_df.name} (%)")
     plt.show()
 
 
@@ -172,22 +184,6 @@ def plot_yearly_return(_yearly_return: pd.Series):
     plt.show()
 
 
-def get_rolling_volatility(df: pd.Series, rolling_window: int = 30) -> pd.Series:
-    # TODO
-    pass
-
-
-def plot_rolling_volatility(_rolling_vol: pd.Series, rolling_window: int = 30):
-    if _rolling_vol.name != 'Rolling Volatility':
-        _rolling_vol = get_rolling_volatility(_rolling_vol, rolling_window)
-
-    # -- plot --
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(_rolling_vol)
-    ax.set(xlabel='Date', ylabel='Volatility (%)', title='Rolling Volatility')
-    plt.show()
-
-
 def get_sharpe_ratio(returns: pd.Series, r_f: float | int = 0) -> float:
     """
     :param returns: the asset returns
@@ -200,7 +196,9 @@ def get_sharpe_ratio(returns: pd.Series, r_f: float | int = 0) -> float:
 
 
 def get_sortino_ratio(returns: pd.Series, r_f: float | int = 0) -> float:
-    down_vol = get_volatility(returns)
+    r_f = num_to_percent(r_f)
+
+    down_vol = get_downside_volatility(returns)
     annual_r_p = get_annual_return(returns) - r_f
 
     return annual_r_p / down_vol
@@ -227,15 +225,26 @@ def get_CVaR(returns: pd.Series, alpha: float = 99, lookback_days: int = None) -
 
 
 # ---- formats ----
-def percent_to_num(returns):  # converts returns to num
-    if max(abs(returns)) < 1:
-        return returns
+def percent_to_num(x):  # converts returns to num
+    # check instance
+    if isinstance(x, pd.Series) or isinstance(x, pd.DataFrame):
+        if max(abs(x)) < 1:
+            return x
+        else:
+            return x / 100
+
+    elif isinstance(x, int) or isinstance(x, float):
+        if abs(x) < 1:
+            return x
+        else:
+            return x / 100
+
     else:
-        return returns / 100
+        raise TypeError(f'does not support this type of input: {type(x)}')
 
 
-def num_to_percent(returns):
-    return 100 * percent_to_num(returns)
+def num_to_percent(x):
+    return 100 * percent_to_num(x)
 
 
 # ---- stats ----