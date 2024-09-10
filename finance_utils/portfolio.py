522626323b58b11f476aece7d0dec7eaa4cbe8a7
henrywzh <henrywzh88@gmail.com>
2024-09-10 20:40:47 +0800

    debug: portfolio rebalancing

--- finance_utils/portfolio.py
+++ finance_utils/portfolio.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import yfinance as yf
 import datetime
+from matplotlib import pyplot as plt
 
 """
 class Portfolio:
@@ -31,7 +32,7 @@ class Portfolio:
             end_date: str = None,
             benchmark: str = None,
             default_weights: list[float] = None,
-            ):
+    ):
         """
         :param data: daily stocks prices, columns: stock names
         :param cash: initial capital
@@ -49,13 +50,13 @@ class Portfolio:
             raise ValueError("number of stocks != length of default_weights")
 
         # -- initialisation --
-        self.shares = pd.DataFrame()
-        self.prices = data
-        self.cash = cash
-        self.cash_df = pd.DataFrame()
-        self.portfolio_values = pd.DataFrame()
-
         self.tickers = data.columns.tolist()
+        self.prices = data
+        self.shares = pd.DataFrame(index=self.prices.index, columns=self.tickers)
+        self.cash = cash
+
+        self.cashes: pd.DataFrame | list = [-1] * len(self.prices)
+        self.portfolio_values: pd.DataFrame | list = [-1] * len(self.prices)
 
         # set benchmark
         self.benchmark = '^GSPC' if benchmark is None else benchmark
@@ -74,6 +75,12 @@ class Portfolio:
         return self.portfolio_values
 
     # -- set up, changes to portfolio settings --
+    def reset(self) -> None:
+        self.cashes = [-1] * len(self.prices)
+        self.portfolio_values = [-1] * len(self.prices)
+
+        self.benchmark_prices = self._get_benchmark()
+
     def set_start_date(self, start_date: str) -> None:
         _start_date = pd.to_datetime(start_date)
         if _start_date >= self.end_date:
@@ -126,57 +133,65 @@ class Portfolio:
         return total_val
 
     def get_portfolio_values(self) -> pd.DataFrame:
-        # TODO:
-        pass
+        return self.portfolio_values
 
-    def get_portfolio_returns(self):
-        # pass the portfolio returns as a pd.Series
-        # TODO:
-        pass
+    def get_portfolio_returns(self) -> pd.DataFrame:
+        # pass the portfolio returns
+        return self.portfolio_values.pct_change()
+
+    def get_all_returns(self) -> pd.DataFrame:
+        # return: daily returns of every stock in the portfolio
+        return self.prices.pct_change()
 
     def get_portfolio_risk(self):
         # TODO:
         pass
 
+    # -- visualisation --
+    def plot(self):
+        self.plot_cum_returns()
+        self.plot_shares()
+        self.plot_weights()
+
+    def plot_cum_returns(self):
+        (self.portfolio_values / self.portfolio_values.iloc[0]).plot(figsize=(10, 6))
+        plt.show()
+
+    def plot_shares(self):
+        self.shares.plot(figsize=(10, 6))
+        plt.show()
+
+    def plot_weights(self):
+        weights: pd.DataFrame = (self.prices * self.shares) / self.portfolio_values.values
+        weights.plot(figsize=(10, 6))
+        plt.show()
+
     # -- portfolio management --
-    def rebalance_portfolio(self) -> None:
+
+    def rebalance_portfolio(self, period: int = 20) -> None:
         # return: the daily value of the portfolio from start to end
-        stock_data = self.prices
-        stock_data = stock_data.reindex(columns=self.tickers)  # ! Key to mantain structure
-
-        shares_df = pd.DataFrame(index=stock_data.index, columns=self.tickers)
-        capitals = []
-        cashes = []
-        ind = 0
-
-        for i in stock_data.index:
-            if ind == 0:
-                # initial set up
-                capitals.append(self.cash)
+        for i, d in enumerate(self.prices.index):
+            if i == 0:
+                self.portfolio_values[i] = self.cash
                 for s, w in zip(self.tickers, self.weights):
-                    shares_df.loc[i, s] = np.floor(capitals[0] * np.array(w) / stock_data[s].loc[i])
+                    self.shares.loc[d, s] = np.floor(self.portfolio_values[i] * np.array(w) / self.prices[s].iloc[i])
 
-                cashes.append(self.cash - (shares_df.loc[i] * stock_data.loc[i]).sum())
-                ind += 1
+                self.cashes[i] = self._compute_cash(i)
                 continue
 
-            capitals.append((stock_data.iloc[ind] * shares_df.iloc[ind - 1]).sum() + cashes[ind - 1])
+            self.portfolio_values[i] = self._compute_values(i)
 
-            if ind % 30 == 0:
-                curr_shares = np.floor(capitals[ind] * np.array(self.weights) / stock_data.loc[i])
-                shares_df.loc[i] = curr_shares
+            if i % period == 0:
+                self.shares.loc[d] = self._compute_shares(i)
             else:
-                shares_df.loc[i] = shares_df.iloc[ind - 1]
+                self.shares.loc[d] = self.shares.iloc[i - 1]
 
-            cashes.append(capitals[ind] - (shares_df.loc[i] * stock_data.loc[i]).sum())
-            ind += 1
+            self.cashes[i] = self._compute_cash(i)
 
-        portfolio_values = pd.DataFrame(capitals, columns=['Value'], index=self.prices.index)
+        self.portfolio_values = pd.DataFrame(self.portfolio_values, columns=['Value'], index=self.prices.index)
+        self.cashes = pd.DataFrame(data=self.cashes, columns=['Cash'], index=self.prices.index)
 
-        self.portfolio_values = portfolio_values
-        self.cash_df = pd.DataFrame(data=cashes, columns=['Cash'], index=self.prices.index)
-
-    def optimise_portfolio(self):
+    def optimise_portfolio(self) -> list:
         # TODO:
         pass
 
@@ -193,14 +208,26 @@ class Portfolio:
         # TODO: What if no stock data for the beginning?
         return _df
 
-    def _compute_shares(self, i):
-        np.floor(capitals[ind] * np.array(self.weights) / stock_data.loc[i])
-        # TODO: compute shares according to weights and current capital
-        pass
+    def _compute_values(self, i: int) -> float:
+        values = (self.prices.iloc[i] * self.shares.iloc[i - 1]).sum() + self.cashes[i - 1]
+
+        return values
+
+    def _compute_shares(self, i: int) -> pd.Series:
+        shares = np.floor(self.portfolio_values[i] * np.array(self.weights) / self.prices.iloc[i])
+
+        return shares
+
+    def _compute_cash(self, i: int) -> float:
+        cash = self.portfolio_values[i] - (self.shares.iloc[i] * self.prices.iloc[i]).sum()
+        return cash
 
     def _compute_weights(self, i: int | str) -> list:
-        curr_vals = self.shares.iloc[i] * self.prices.iloc[i] if isinstance(i, int) \
-            else self.shares.loc[i] * self.prices.loc[i]
+        if isinstance(i, int):
+            curr_vals = self.shares.iloc[i] * self.prices.iloc[i]
+        else:
+            curr_vals = self.shares.loc[i] * self.prices.loc[i]
+
         return list(curr_vals / self.get_value(i))
 
     def _check_weights(self) -> bool:
@@ -208,3 +235,41 @@ class Portfolio:
 
     def _reset_weights(self) -> None:
         self.weights = [1 / len(self.tickers) for t in self.tickers]
+
+# def rebalance_portfolio2(self) -> None:
+#     # return: the daily value of the portfolio from start to end
+#     stock_data = self.prices
+#     stock_data = stock_data.reindex(columns=self.tickers)  # ! Key to mantain structure
+#
+#     shares_df = pd.DataFrame(index=stock_data.index, columns=self.tickers)
+#     capitals = [-1] * len(self.prices.index)
+#     cashes = [-1] * len(self.prices.index)
+#     ind = 0
+#
+#     for i in stock_data.index:
+#         if ind == 0:
+#             # initial set up
+#             capitals[ind] = self.cash
+#             for s, w in zip(self.tickers, self.weights):
+#                 shares_df.loc[i, s] = np.floor(capitals[0] * np.array(w) / stock_data[s].loc[i])
+#
+#             cashes[ind] = self.cash - (shares_df.loc[i] * stock_data.loc[i]).sum()
+#             ind += 1
+#             continue
+#
+#         capitals[ind] = (stock_data.iloc[ind] * shares_df.iloc[ind - 1]).sum() + cashes[ind - 1]
+#
+#         if ind % 30 == 0:
+#             curr_shares = np.floor(capitals[ind] * np.array(self.weights) / stock_data.loc[i])
+#             shares_df.loc[i] = curr_shares
+#         else:
+#             shares_df.loc[i] = shares_df.iloc[ind - 1]
+#
+#         cashes[ind] = capitals[ind] - (shares_df.loc[i] * stock_data.loc[i]).sum()
+#         ind += 1
+#
+#     portfolio_values = pd.DataFrame(capitals, columns=['Value'], index=self.prices.index)
+#
+#     self.portfolio_values = portfolio_values
+#     self.cashes = pd.DataFrame(data=cashes, columns=['Cash'], index=self.prices.index)
+#     self.shares = shares_df
